19a5d2616b548feb2da9fad719353ff7ed34e48d
gbn <18843402198@163.com>
2021-05-25 16:02:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bigscreen">
+    <div class="row">
+      <header>
+        <h1>天气监测</h1>
+      </header>
+      <weather></weather>
+      <so></so>
+    </div>
+  </div>
+</template>
+
+<script>
+import weather from "./components/weather";
+import so from "./components/so";
+export default {
+  components:{weather,so}
+}
+</script>
+
+<style >
+body{
+  background-image: url("./assets/bg.png");
+}
+
+h1{
+  text-align: center;
+  color: aliceblue;
+  /*background-image: url("./assets/head.png");*/
+}
+</style>
