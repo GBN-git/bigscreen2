3e7f3426bd0ef746e48907a5eb37ac154c457f98
gbn <18843402198@163.com>
2021-05-25 18:15:43 +0800

    init

---
+++ src/components/so.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="row">
+    <div class="col-md-6">
+      <h3 class="h3">1</h3>
+      <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
+    </div>
+    <div class="col-md-6">
+      <h3 class="h3">1</h3>
+      <div id="chart7" :style="{width: '300px', height: '300px'}"></div>
+    </div>
+  </div>
+
+</template>
+<script>
+import * as echarts from "echarts";
+
+export default {
+  name: "so",
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted(){
+    this.drawLine();
+
+  },
+  methods: {
+    drawLine(){
+
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = echarts.init(document.getElementById('myChart'))
+      // 绘制图表
+      myChart.setOption({
+        tooltip: {},
+        xAxis: {
+          data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      }
+      );
+
+      let chart7 = echarts.init(document.getElementById('chart7'))
+      // 绘制图表
+      chart7.setOption({
+            tooltip: {},
+            xAxis: {
+              data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+              name: '销量',
+              type: 'bar',
+              data: [5, 20, 36, 10, 10, 20]
+            }]
+          }
+      );
+    }
+  }
+}
+
+</script>
+
+<style>
+@import "../css/so.css";
+</style>
---
+++ src/components/weather.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="col-md-12" id="">
+        <h3 class="h3">1</h3>
+        <div id="chart1" :style="{width: '300px', height: '300px'}"></div>
+      </div>
+      <div class="col-md-12">
+        <h3 class="h3">1</h3>
+
+      </div>
+    </div>
+    <div class="col-md-4">
+      <h3 class="h3">1</h3>
+
+
+    </div>
+    <div class="col-md-4">
+      <div class="col-md-12">
+        <h3 class="h3">1</h3>
+
+      </div>
+      <div class="col-md-12">
+        <h3 class="h3">1</h3>
+
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import * as echarts from "echarts";
+export default {
+  name: "weather",
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted(){
+    this.drawLine();
+  },
+  methods: {
+    drawLine(){
+      // 基于准备好的dom，初始化echarts实例
+      let chart1 = echarts.init(document.getElementById('chart1'))
+      // 绘制图表
+      chart1.setOption({
+        tooltip: {},
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line',
+          smooth: true
+        }]
+      });
+    }
+  }
+
+}
+
+</script>
+<style>
+@import "../css/weather.css";
+</style>
